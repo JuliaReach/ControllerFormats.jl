e2a20b04ce61b9381343045734f93782a8f0bed7
schillic <git@christianschilling.net>
2023-04-30 20:11:03 +0200

    test Sherlock writer

--- test/FileFormats/Sherlock.jl
+++ test/FileFormats/Sherlock.jl
@@ -7,3 +7,13 @@ N = read_Sherlock(file)
 @test dim_in(N) == 2
 @test dim_out(N.layers[1]) == 2
 @test dim_out(N) == 1
+
+
+# write network back to file and re-read it
+file = joinpath(@__DIR__, "sample_Sherlock_output")
+
+write_Sherlock(N, file)
+N2 = read_Sherlock(file)
+rm(file)
+
+@test N == N2
