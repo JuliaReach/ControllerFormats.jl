b94136251e2d6d4149022185060a2bb70b5d1f3c
schillic <git@christianschilling.net>
2023-04-30 19:03:06 +0200

    extend MAT parser: nested dictionary; 1x1 matrix as number

--- test/FileFormats/MAT.jl
+++ test/FileFormats/MAT.jl
@@ -5,3 +5,20 @@ file = joinpath(@__DIR__, "sample_MAT.mat")
 N = read_MAT(file, act_key="act_fcns")
 
 @test length(N.layers) == 4
+
+
+# alternative file with 4D weights
+file = joinpath(@__DIR__, "sample_MAT2.mat")
+
+# parse file
+N = read_MAT(file, act_key="act_fcns")
+
+@test length(N.layers) == 4
+
+
+# alternative file with nested dictionary
+file = joinpath(@__DIR__, "sample_MAT3.mat")
+
+N = read_MAT(file, act_key="activation_fcns", net_key="network")
+
+@test length(N.layers) == 4
