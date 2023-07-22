4c80d7047fca5b9a3ce0db7d1d1c6ab02a14ea81
schillic <git@christianschilling.net>
2023-07-22 18:11:22 +0200

    fix POLAR format: end pattern

--- src/FileFormats/POLAR.jl
+++ src/FileFormats/POLAR.jl
@@ -15,6 +15,15 @@ A [`FeedforwardNetwork`](@ref).
 
 The POLAR format uses the same parameter format as Sherlock (see
 [`read_Sherlock`](@ref)) but allows for general activation functions.
+
+In addition, the last two lines are:
+```
+0.0
+1.0
+```
+
+The reference parser and writer can be found
+[here](https://github.com/ChaoHuang2018/POLAR_Tool/blob/8df333a59321f45227dafc87c367779783b6166c/POLAR/neuralnetwork.py).
 """
 function read_POLAR(filename::String)
     function read_activations(io, n_layer_ops)
@@ -24,7 +33,16 @@ function read_POLAR(filename::String)
 
     layer_type = DenseLayerOp{<:ActivationFunction,Matrix{Float32},Vector{Float32}}
 
-    return _read_Sherlock_POLAR(filename, read_activations, layer_type)
+    return _read_Sherlock_POLAR(filename, read_activations, layer_type, _read_end_POLAR)
+end
+
+function _read_end_POLAR(io)
+    line1 = readline(io)
+    line2 = readline(io)
+    if line1 != "0.0" || line2 != "1.0"
+        throw(ArgumentError("the POLAR format requires to end with two lines " *
+                            "containing `0.0` and `1.0`"))
+    end
 end
 
 """
@@ -42,7 +60,7 @@ Write a neural network to a file in POLAR format.
 `nothing`. The network is written to the output file.
 """
 function write_POLAR(N::FeedforwardNetwork, filename::String)
-    return _write_Sherlock_POLAR(N, filename, _write_activation_POLAR)
+    return _write_Sherlock_POLAR(N, filename, _write_activation_POLAR, _write_end_POLAR)
 end
 
 function _write_activation_POLAR(io, layer)
@@ -51,6 +69,12 @@ function _write_activation_POLAR(io, layer)
         throw(ArgumentError("unsupported activation function `$(typeof(layer.activation))`"))
     end
     println(io, act)
+    return nothing
+end
+
+function _write_end_POLAR(io)
+    println(io, "0.0")
+    println(io, "1.0")
     return nothing
 end
 
