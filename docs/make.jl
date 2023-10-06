2017f5b4553b2f06ed4eedf672e047cd393da2c7
schillic <git@christianschilling.net>
2023-10-06 23:12:51 +0200

    support/use Documenter v1

--- docs/make.jl
+++ docs/make.jl
@@ -7,12 +7,12 @@ makedocs(; sitename="ControllerFormats.jl",
          modules=[ControllerFormats],
          format=Documenter.HTML(; prettyurls=get(ENV, "CI", nothing) == "true",
                                 assets=["assets/aligned.css"]),
+         pagesonly=true,
          pages=["Home" => "index.md",
                 "Library" => ["ControllerFormats module" => "lib/ControllerFormats.md",
                               "Architecture module" => "lib/Architecture.md",
                               "FileFormats module" => "lib/FileFormats.md"],
-                "About" => "about.md"],
-         strict=true)
+                "About" => "about.md"])
 
 deploydocs(; repo="github.com/JuliaReach/ControllerFormats.jl.git",
            push_preview=true)
