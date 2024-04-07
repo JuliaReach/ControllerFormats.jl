d31df1b0ebfbb609cdd9d62aafa6c779b5d473ec
schillic <git@christianschilling.net>
2024-04-07 22:36:01 +0200

    rename function

--- src/Architecture/LayerOps/DenseLayerOp.jl
+++ src/Architecture/LayerOps/DenseLayerOp.jl
@@ -73,7 +73,7 @@ dim_in(L::DenseLayerOp) = size(L.weights, 2)
 
 dim_out(L::DenseLayerOp) = length(L.bias)
 
-function load_Flux_convert_layer()
+function load_Flux_convert_Dense_layer()
     return quote
         function Base.convert(::Type{DenseLayerOp}, layer::Flux.Dense)
             act = get(activations_Flux, layer.σ, nothing)
--- src/Architecture/init.jl
+++ src/Architecture/init.jl
@@ -2,7 +2,7 @@
 function __init__()
     @require Flux = "587475ba-b771-5e3f-ad9e-33799f191a9c" begin
         eval(load_Flux_activations())
-        eval(load_Flux_convert_layer())
+        eval(load_Flux_convert_Dense_layer())
         eval(load_Flux_convert_network())
     end
 end
