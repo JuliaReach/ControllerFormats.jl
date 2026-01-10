aa46cd4b3313b1291a685e55645cad72273b3342
schillic <git@christianschilling.net>
2026-01-10 21:41:55 +0100

    use ExplicitImports in tests

--- test/quality_assurance.jl
+++ test/quality_assurance.jl
@@ -1,5 +1,23 @@
 using ControllerFormats, Test
-import Aqua
+import Aqua, ExplicitImports
+
+@testset "ExplicitImports tests" begin
+    ignores = (:rtoldefault, :Umlaut, :load, :load_file)
+    @test isnothing(ExplicitImports.check_all_explicit_imports_are_public(ControllerFormats;
+                                                                          ignore=ignores))
+    ignores = (:Umlaut,)
+    @test isnothing(ExplicitImports.check_all_explicit_imports_via_owners(ControllerFormats;
+                                                                          ignore=ignores))
+    ignores = (:ONNXCtx, :ProtoDecoder, :decode, :onnx_gemm)
+    @test isnothing(ExplicitImports.check_all_qualified_accesses_are_public(ControllerFormats;
+                                                                            ignore=ignores))
+    ignores = (:ProtoDecoder, :decode)
+    @test isnothing(ExplicitImports.check_all_qualified_accesses_via_owners(ControllerFormats;
+                                                                            ignore=ignores))
+    @test isnothing(ExplicitImports.check_no_implicit_imports(ControllerFormats))
+    @test isnothing(ExplicitImports.check_no_self_qualified_accesses(ControllerFormats))
+    @test isnothing(ExplicitImports.check_no_stale_explicit_imports(ControllerFormats))
+end
 
 @testset "Aqua tests" begin
     Aqua.test_all(ControllerFormats)
